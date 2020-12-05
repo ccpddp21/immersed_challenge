5e59611ef67d2651e3c0361e74c3db6f42acb890
ccpddp21 <christopher.peele21@gmail.com>
2020-12-05 17:28:00 -0500

    Added 'Project Cloner' package as dev dependency.

---
+++ Immersed Challenge/Assets/_Code/Components/Networking/InitializePlayer.cs
@@ -0,0 +1,22 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializePlayer : NetworkedBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!IsLocalPlayer)
+        {
+            Destroy(transform.GetComponentInChildren<Camera>().gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
