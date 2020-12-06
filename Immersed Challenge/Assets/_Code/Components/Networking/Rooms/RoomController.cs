3772cc1a47da4dbbcde87c5df352170a8d330be2
ccpddp21 <christopher.peele21@gmail.com>
2020-12-06 16:52:37 -0500

    Rename file.

--- Immersed Challenge/Assets/_Code/Components/Networking/Rooms/RoomController.cs
+++ Immersed Challenge/Assets/_Code/Components/Networking/Rooms/RoomController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomManager : MonoBehaviour
+public class RoomController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
