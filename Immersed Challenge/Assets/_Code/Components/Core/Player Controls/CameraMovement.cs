2720e5fce4ee70ed710f40ff70226b5749b6b711
ccpddp21 <christopher.peele21@gmail.com>
2020-12-06 18:48:16 -0500

    Implemented Character move and look controls.

---
+++ Immersed Challenge/Assets/_Code/Components/Core/Player Controls/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private float _yawAngle;
+    [SerializeField] private float _pitchAngle;
+    private float _yawValue, _pitchValue;
+
+    void Awake()
+    {
+        if (_playerCamera == null)
+        {
+            _playerCamera = this.GetComponentInChildren<Camera>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _yawAngle = _pitchAngle = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _yawValue = Input.GetAxis("Yaw");
+        _pitchValue = Input.GetAxis("Pitch");
+
+        if (_yawValue != 0)
+        {
+            this.transform.Rotate(new Vector3(0, 1, 0) * _yawAngle * _yawValue * Time.deltaTime);
+        }
+
+        if (_pitchValue != 0)
+        {
+            _playerCamera.transform.Rotate(new Vector3(1, 0, 0) * _pitchAngle * _pitchValue * Time.deltaTime);
+        }
+    }
+}
---
+++ Immersed Challenge/Assets/_Code/Components/Core/Player Controls/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _verticalValue, _horizontalValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _verticalValue = Input.GetAxis("Vertical");
+        _horizontalValue = Input.GetAxis("Horizontal");
+
+        if (_verticalValue != 0)
+        {
+            this.transform.localPosition += (this.transform.forward * _verticalValue * Time.deltaTime);
+        }
+        
+        if (_horizontalValue != 0)
+        {
+            this.transform.localPosition += (this.transform.right * _horizontalValue * Time.deltaTime);
+        }
+    }
+}
