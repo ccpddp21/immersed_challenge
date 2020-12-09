bba2bdac9b3ee1334843aa6736628ec6955af367
ccpddp21 <christopher.peele21@gmail.com>
2020-12-09 16:15:30 -0500

    Created UI for Professor desk in Lecture Hall.

--- Immersed Challenge/Assets/_Code/Components/UI/CanvasManager.cs
+++ Immersed Challenge/Assets/_Code/Components/UI/CanvasManager.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
+    public enum OwnerCamera
+    {
+        Active,
+        Professor
+    }
+
+    [SerializeField] private OwnerCamera _ownerCamera;
+
+    private Canvas _thisCanvas;
+    private GameObject _cameraObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_thisCanvas == null)
+        {
+            _thisCanvas = this.GetComponent<Canvas>();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (_thisCanvas.worldCamera == null)
+        {
+            if (_ownerCamera == OwnerCamera.Professor)
+            {
+                _cameraObject = GameObject.FindGameObjectWithTag("Professor Avatar");
+                if (_cameraObject != null)
+                {
+                    _thisCanvas.worldCamera = _cameraObject.GetComponentInChildren<Camera>();
+                }
+            }
+            else if (_ownerCamera == OwnerCamera.Active)
+            {
+                _cameraObject = GameObject.FindGameObjectWithTag("Player Camera");
+                if (_cameraObject != null)
+                {
+                    _thisCanvas.worldCamera = _cameraObject.GetComponent<Camera>();
+                }
+            }
+        }
     }
 }
