4499aecfe223462db5cd95a3c716582a04536f09
ccpddp21 <christopher.peele21@gmail.com>
2020-12-09 15:01:42 -0500

    Created 'AppEvent' and 'AppEventListener' scriptableObjects.

---
+++ Immersed Challenge/Assets/_Code/ScriptableObjects/Events/AppEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "App Event")]
+public class AppEvent : ScriptableObject
+{
+    private List<AppEventListener> listeners = new List<AppEventListener>();
+
+    public void Raise()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(AppEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(AppEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
